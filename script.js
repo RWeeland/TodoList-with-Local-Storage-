d48d473bb18b3738e37c47508c30e1b629e0c02c
RWeeland <143615203+RWeeland@users.noreply.github.com>
2024-08-11 23:58:22 +0200

    Added form functionality

--- script.js
+++ script.js
@@ -7,16 +7,27 @@ const navTodo = document.getElementById("todo");
 const navFeedback = document.getElementById("feedback");
 const navNotes = document.getElementById("notes");
 const navDrills = document.getElementById("drills");
-const todoItem = JSON.parse(localStorage.getItem("data")) || [];
 const currentInput = document.querySelectorAll(".selected-input");
 const inputContainer = document.getElementById("input-container")
+const dialog = document.querySelector("dialog");
+const scrollContainer = document.getElementById("scroll-container");
 let canEdit = 1;
+
+
+//Local Storage for the different containers
+const todoItem = JSON.parse(localStorage.getItem("data")) || [];
+const formItem = JSON.parse(localStorage.getItem("form")) || [];
+
 // Locates item index
 const itemLocation = (item) => todoItem.findIndex((el)=> el.id === item.parentElement.id);
+const itemLocationForm = (item) => formItem.findIndex((el)=> el.id === item.parentElement.id);
 //Saves Storage
 const saveStorage = () => { localStorage.setItem("data", JSON.stringify(todoItem))
 renderTodo();}
 
+const saveForm = () => {localStorage.setItem("form", JSON.stringify(formItem))
+    displayFormItems();
+};
 //drills
 
 const drills = ["Cut the deck at a random point, turn over the top card and riffle stack the value of the top card on top of the card",
@@ -43,33 +54,43 @@ localStorage.setItem("data", JSON.stringify(todoItem))
 //Handles removing an item
 const removeParent = (item)=>{
         const index = itemLocation(item);
-        item.parentElement.remove();
     todoItem.splice(index,1)
-    saveStorage()
+    saveStorage();
+    }
+
+    const removeParentForm = (item)=>{
+        const index = itemLocationForm(item);
+    formItem.splice(index,1)
+    displayFormItems();
+    saveForm()
     }
 
 //Handles editing and confirming
-    const editItem1 = (item)=>{
-        if (canEdit){
-        canEdit --;
+const editItem1 = (item)=>{
+    if (canEdit) {
+        canEdit--;
         const index = itemLocation(item);
-         const parentEl = item.parentElement;
-         const {id, task} =  todoItem[index];
-        parentEl.innerHTML= `<div id="${id}" class="todo-item">
-        <input type="text" id="${id}-input" value="${task}" class="selected-input">
-        <button onclick ="editItem2(${id})" id="${id}-edit" class="edit-btn fa-solid fa-check fa-lg btn"></button>
-    </div>`
-    const newInput = document.getElementById(`${id}-input`)
-    newInput.focus();
-    newInput.setSelectionRange(newInput.value.length, newInput.value.length)
-    newInput.addEventListener("keydown",(e)=>{
-         if(e.key ==="Enter"){
-             editItem2(id)
-         } 
-    })
-        } else {return};
+        const parentEl = item.parentElement;
+        const {id, task} = todoItem[index];
+        
+        parentEl.innerHTML = `
+        <div id="${id}" class="todo-item">
+            <input type="text" id="${id}-input" value="${task}" class="selected-input">
+            <button onclick="editItem2('${id}')" id="${id}-edit" class="edit-btn fa-solid fa-check fa-lg btn"></button>
+        </div>`;
+        
+        const newInput = document.getElementById(`${id}-input`);
+        newInput.focus();
+        newInput.setSelectionRange(newInput.value.length, newInput.value.length);
+        newInput.addEventListener("keydown", (e) => {
+            if (e.key === "Enter") {
+                editItem2(id);
+            }
+        });
+    } else {
+        return;
     }
-
+};
      const editItem2 = (item)=>{
         const index = todoItem.findIndex((el)=> el.id === item);
          const {id, task, checked} =  todoItem[index];
@@ -167,18 +188,23 @@ userInput.addEventListener("keydown",(e)=>{
      }
 
      navTodo.addEventListener("click", ()=>{
-        inputContainer.classList.remove("hidden")
+        inputContainer.classList.remove("hidden");
+        scrollContainer.innerHTML = '';
         renderTodo()
+        
      })
 
      navFeedback.addEventListener("click", ()=>{
         inputContainer.classList.add("hidden");
         renderForm();
+        
      })
 
      navDrills.addEventListener("click",()=>{
         inputContainer.classList.add("hidden");
+        scrollContainer.innerHTML = '';
         renderDrill();
+        
      })
 
      
@@ -186,7 +212,8 @@ userInput.addEventListener("keydown",(e)=>{
  //render for the form
 
  //Add form atributes where it will save and make sure that the form will display entries below, maybe with a pop-up?
-
+let dateForm, eventForm, whatWentForm, improveForm, memmorableForm, weirdForm, notesForm, submitForm;
+ 
 const renderForm = ()=>{
     displayTodo.innerHTML = ""
     displayTodo.innerHTML= `<div id="form">
@@ -205,11 +232,105 @@ const renderForm = ()=>{
                             <input id="weird" type="text">
                             <label for="random-notes">Random notes</label>
                             <input id="random-notes" type="text">
-                            <button type="submit" id="submitForm">Submit</button>
+                            <button type="button" id="submit-form">Submit</button>
                         </form>
                        </div>`
+
+    dateForm = document.getElementById("date");
+    eventForm = document.getElementById("event");
+    whatWentForm = document.getElementById("what-went");
+    improveForm = document.getElementById("improve");
+    memmorableForm = document.getElementById("memmorable");
+    weirdForm = document.getElementById("weird");
+    notesForm = document.getElementById("random-notes");
+    submitForm = document.getElementById("submit-form");
+    displayFormItems();
+
+
+         submitForm.addEventListener("click", (e)=>{
+            e.preventDefault();
+            getForm();
+            saveForm;
+            console.log(formItem);
+            displayFormItems();
+            document.querySelectorAll("form input").forEach(input => input.value = '')
+                    
+                    })
+}   
+
+const getForm = () =>{
+    const formObj = {
+        id: `${eventForm.value.split(" ").join("-")}-${Date.now()}`,
+        date: dateForm.value,
+        eventF: eventForm.value,
+        whatWent: whatWentForm.value,
+        improve: improveForm.value,
+        memmorable: memmorableForm.value,
+        weird: weirdForm.value,
+        notes: notesForm.value,
+    }
+    formItem.unshift(formObj);
+    saveForm();
 }
 
+const displayFormItems = () =>{
+    scrollContainer.innerHTML='';
+    formItem.forEach(({id, date, eventF} )=> {
+        scrollContainer.innerHTML += `
+        <div id=${id} class="form-filled">
+                                <h2 class="info-form">${date}</h2>
+                                <h2 class="info-form">${eventF}</h2>
+                                <button onclick="renderModal(this)" class="show-form">Show More</button>
+                                <button onclick="removeParentForm(this)">Delete</button>
+                            </div>
+        `
+    });
+};
+
+
+const renderModal = (item) => {
+    const index = itemLocationForm(item);
+    console.log(index)
+    const currentFormObj = formItem[index];
+    const {id, date, eventF, whatWent, improve, memmorable, weird, notes} = currentFormObj;
+    dialog.innerHTML = `
+    <div id="${id}">
+     <h3 class="form-header"> Date</h3>
+              <p class="form-text">${date}</p>
+              <h3 class="form-header">Event</h3>
+              <p class="form-text">${eventF}</p>
+              <h3 class="form-header">What went well? </h3>
+              <p class="form-text">${whatWent}</p>
+              <h3 class="form-header">What do you want to improve? </h3>
+              <p class="form-text">${improve}</p>
+              <h3 class="form-header">Most Memmorable moment</h3>
+              <p class="form-text">${memmorable}</p>
+              <h3 class="form-header"> Weird Moments?</h3>
+              <p class="form-text">${weird} </p>
+              <h3 class="form-header">Notes</h3>
+              <p class="form-text">${notes}</p>
+              <button onclick="dialog.close()" id="close-modal">Close</button>
+    </div>
+    `
+    dialog.showModal();
+    dialog.addEventListener("click", e => {
+        const dialogDimensions = dialog.getBoundingClientRect()
+        if (
+          e.clientX < dialogDimensions.left ||
+          e.clientX > dialogDimensions.right ||
+          e.clientY < dialogDimensions.top ||
+          e.clientY > dialogDimensions.bottom
+        ) {
+          dialog.close()
+        }
+      })
+}
+
+
+
+
+
+//DRILL
 const renderDrill = ()=>{
     displayTodo.innerText = "";
     displayTodo.innerHTML = `
@@ -227,3 +348,4 @@ const renderDrill = ()=>{
 
     })
 }
+
